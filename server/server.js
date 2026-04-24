65e7c46c4c843d704875ee5cd5731b8f79ca4de5
gudidevuniumeshgoud-lab <gudidevuniumeshgoud@gmail.com>
2026-04-24 20:56:11 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const connectDB = require('./config/db');
 
 const authRoutes = require('./routes/authRoutes');
